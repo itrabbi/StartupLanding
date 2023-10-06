f106f6f77026fcbf3eb6d805969e94cb86c1804d
itrabbi <hassan.rabbi6321@gmail.com>
2023-10-06 22:24:37 +0600

    Setup

---
+++ script.js
@@ -0,0 +1,9 @@
+var resMobile = document.getElementById('resMobile');
+
+function openmenu() {
+    resMobile.style.right = "0";
+}
+function closemenu() {
+    resMobile.style.right = "-250px";
+    resMobile.style.display = "visble";
+}
